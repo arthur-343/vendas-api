f31da60cd4f83ce7d02897a55bf9f584d6365090
arthur-343 <arthurssace@gmail.com>
2025-05-15 17:16:26 -0300

    Primeiro commit do vendas-api

---
+++ src/main/java/com/vendas/negocio_digital/NegocioDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.vendas.negocio_digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NegocioDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NegocioDigitalApplication.class, args);
+	}
+
+}
